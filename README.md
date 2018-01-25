2196468ca47a932cbe9ffe3e22f626d125195f77
Unwana Essien <unwana.essien@outlook.com>
2018-01-25 17:44:35 +0100

    base setup complete and working

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # wbpk
+
+This is an attempt to create a template that I can use whenever I want to start a new project. 
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  // set this to your entry point
+  entry: "./src/index.js",
+
+  // change this to your output path
+  output: {
+    path: path.resolve(__dirname,'/dist/js/'),
+    filename: "bundle.js",
+    publicPath: "/assets/"
+  },
+
+  // create a map file for debugging
+  devtool: 'source-map',
+
+  // configure the loaders
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel",
+        query: {
+          presets: ['es2015', 'react', 'stage-3'],
+          compact: false
+        }
+      }
+    ]
+  },
+
+  ///////////  uncomment this for production ////////////////
+  // plugins: [
+  //   new webpack.optimize.UglifyJsPlugin({
+  //     compress: {
+  //       warnings: false
+  //     }
+  //   }),
+  //   new webpack.DefinePlugin({
+  //     'process.env': {'NODE_ENV': JSON.stringify('production')}
+  //   })
+  // ],////////////////////////////////////////////////////////
+
+  watch: true // change this to true to keep webpack running
+};
